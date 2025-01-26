7a418ede457f1de87cfc1cdada5a656a0c80aab8
zewais <arch.ziad.ewais@gmail.com>
2025-01-26 17:50:31 -0500

    initial commit

---
+++ lab-TDD-starter.js
@@ -0,0 +1,82 @@
+/*
+Lab 3 - simple TDD with math functions
+Starter file
+Create the following functions as per the instructions below.
+Read the test cases carefully and write the functions accordingly.
+NOTE: The name of the functions have
+to match the exported names mentioned at the end of this file
+in order to pass the test cases.
+To run the test cases, run the following command on your terminal:
+node --test
+*/
+
+//add function
+const add = () => {
+  //write your code here
+  return; //return the result
+};
+
+//subtract function
+const subtract = () => {
+  //write your code here
+  return; //return the result
+};
+
+//multiply function
+const multiply = () => {
+  //write your code here
+  return; //return the result
+};
+
+//divide function
+const divide = () => {
+  //write your code here
+  return; //return the result
+};
+
+//modulas function
+const modulas = () => {
+  //write your code here
+  return; //return the result
+};
+
+//power function
+const power = () => {
+  //write your code here
+  return; //return the result
+};
+
+//square function
+const square = () => {
+  //write your code here
+  return; //return the result
+};
+
+//floor function
+const floor = () => {
+  //write your code here
+  return; //return the result
+};
+
+//ceiling function
+const ceiling = () => {
+  //write your code here
+  return; //return the result
+};
+
+////////////////////////////////////////
+
+//export functions
+//Please do not change the following line.
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  modulas,
+  power,
+  square,
+  floor,
+  ceiling,
+};
