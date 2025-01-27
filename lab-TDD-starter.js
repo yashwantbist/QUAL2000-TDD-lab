b33a4580d4ad53a7ff842408341545b6ddd19d4c
zewais <arch.ziad.ewais@gmail.com>
2025-01-26 22:21:03 -0500

    fix starter file

--- lab-TDD-starter.js
+++ lab-TDD-starter.js
@@ -12,55 +12,66 @@ node --test
 */
 
 //add function
-const add = () => {
+const add = (a, b) => {
+  //ex: add(3,5) should return 5
   //write your code here
   return; //return the result
 };
 
 //subtract function
-const subtract = () => {
+const subtract = (a, b) => {
+  //ex: subtract(5,9) should return -4
   //write your code here
   return; //return the result
 };
 
 //multiply function
-const multiply = () => {
+const multiply = (a, b) => {
+  //ex: multiply (12, 9) should return 108
   //write your code here
   return; //return the result
 };
 
 //divide function
-const divide = () => {
+const divide = (a, b) => {
+  //ex: divide(55, 5) should return 11
+  //Make sure to test for zero division should throw and error
   //write your code here
   return; //return the result
 };
 
 //modulas function
-const modulas = () => {
+const modulas = (a, b) => {
+  //ex: modulas(17,4) should return 1
   //write your code here
   return; //return the result
 };
 
 //power function
-const power = () => {
+const power = (a, b) => {
+  //ex: power(2, 5) should return 32
   //write your code here
   return; //return the result
 };
 
 //square function
-const square = () => {
+const squareRoot = (a) => {
+  //Make sure that squareRoot a negative number should throw an error
+  //ex: squareRoot(25) should return 5
   //write your code here
   return; //return the result
 };
 
 //floor function
-const floor = () => {
+const floor = (a) => {
+  //ex: floor(15.3) should return 15
   //write your code here
   return; //return the result
 };
 
 //ceiling function
-const ceiling = () => {
+const ceiling = (a) => {
+  //ex: ceiling(15.3) should return 16
   //write your code here
   return; //return the result
 };
@@ -77,7 +88,7 @@ module.exports = {
   divide,
   modulas,
   power,
-  square,
+  squareRoot,
   floor,
   ceiling,
 };
